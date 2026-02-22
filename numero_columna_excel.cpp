b1b334854d891703d399fcb9c586ae1dcd2655c9
Juan David <juangidd@outlook.es>
2026-02-22 13:55:56 -0500

    Agrego solucion mejor tiempo y numero columna

---
+++ numero_columna_excel.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        long result = 0;
+        
+        for (char c : columnTitle) {
+            result = result * 26 + (c - 'A' + 1);
+        }
+        
+        return result;
+    }
+};
