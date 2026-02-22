0bdc2a1d5f569d05b95adfd164f1d9ad03b98932
Juan David <juangidd@outlook.es>
2026-02-22 14:39:38 -0500

    Se agrega main para probar Collatz

--- collatz_conjecture.cpp
+++ collatz_conjecture.cpp
@@ -1,21 +1,27 @@
 #include <iostream>
-#include <string>
-
 using namespace std;
-class Solution {
-public:
-    int collatzSteps(int n) {
-        int steps = 0;
-        
-        while (n != 1) {
-            if (n % 2 == 0) {
-                n /= 2;
-            } else {
-                n = 3 * n + 1;
-            }
-            steps++;
-        }
-        
-        return steps;
+
+int collatzSteps(long long n) {
+    int steps = 0;
+
+    while (n != 1) {
+        if (n % 2 == 0)
+            n /= 2;
+        else
+            n = 3 * n + 1;
+
+        steps++;
     }
-};
+
+    return steps;
+}
+
+int main() {
+    long long n;
+    cout << "Ingrese un numero positivo: ";
+    cin >> n;
+
+    cout << "Numero de pasos: " << collatzSteps(n) << endl;
+
+    return 0;
+}
